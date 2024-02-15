f803fa3a9027cc3106b663830602087bee624017
gaetano-94 <gaetano.panico1994@gmail.com>
2024-02-15 15:51:36 +0100

    feat: header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,7 +5,19 @@ export default {
 </script>
 
 <template>
-  <header></header>
+  <header class="bg-primary text-white">
+    <div class="container d-flex justify-content-between py-2">
+      <div>Logo</div>
+      <nav>
+        <ul class="list-unstyled m-0 p-0 gap-4 d-flex">
+          <li>Blog</li>
+          <li>Blog</li>
+          <li>Blog</li>
+          <li>Blog</li>
+        </ul>
+      </nav>
+    </div>
+  </header>
 </template>
 
 <style scoped lang="scss"></style>
