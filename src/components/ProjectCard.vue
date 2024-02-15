ee12e0b31557baac131245bb81053971ad0f3de8
gaetano-94 <gaetano.panico1994@gmail.com>
2024-02-15 16:28:32 +0100

    feat: creazione di un file dedicato alle card dei project

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  name: 'Main',
+  props: ['projects'],
+};
+</script>
+
+<template>
+  <main>
+    <div class="container py-3 text-uppercase"></div>
+  </main>
+</template>
+
+<style scoped lang="scss"></style>
