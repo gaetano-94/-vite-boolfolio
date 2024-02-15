5bd33c15dcf19ee39a047bb9c8104e5a018d975c
gaetano-94 <gaetano.panico1994@gmail.com>
2024-02-15 16:07:46 +0100

    feat: chiamata api con axios e stampare in console tutti i risultati del nostro progetto

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,37 @@
 import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
 import AppFooter from './components/AppFooter.vue';
+import axios from 'axios';
 
 export default {
   name: 'Container',
+  data() {
+    return {
+      baseUrl: 'http://127.0.0.1:8000',
+      apiUrl: {
+        projects: '/api/projects',
+      },
+    };
+  },
   components: {
     AppHeader,
     AppMain,
     AppFooter,
+  },
+  methods: {
+    getProjects() {
+      axios
+        .get(this.baseUrl + this.apiUrl.projects)
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+  created() {
+    this.getProjects();
   },
 };
 </script>
