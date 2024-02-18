bd87315b39e25c3d5f023c089b221cb579c874ac
gaetano-94 <gaetano.panico1994@gmail.com>
2024-02-18 22:38:07 +0100

    feat: import not-found and project-detail

--- src/router.js
+++ src/router.js
@@ -3,6 +3,8 @@ import { createRouter, createWebHistory } from 'vue-router';
 import AppHome from './pages/AppHome.vue';
 import ProjectList from './pages/ProjectList.vue';
 import AppAbout from './pages/AppAbout.vue';
+import NotFound from './pages/NotFound.vue';
+import ProjectDetail from './pages/ProjectDetail.vue';
 
 const router = createRouter({
   history: createWebHistory(),
@@ -18,9 +20,19 @@ const router = createRouter({
       component: ProjectList,
     },
     {
+      path: '/blog/:slug',
+      name: 'single-project',
+      component: ProjectDetail,
+    },
+    {
       path: '/about',
       name: 'about',
       component: AppAbout,
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'not-found',
+      component: NotFound,
     },
   ],
 });
