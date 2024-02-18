68fa9703a308570a29861d8f1f7a4ad2ba7ecad8
gaetano-94 <gaetano.panico1994@gmail.com>
2024-02-18 13:15:12 +0100

    feat: create pages

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,12 @@
 <script>
 import AppHeader from './components/AppHeader.vue';
-import AppMain from './components/AppMain.vue';
 import AppFooter from './components/AppFooter.vue';
-import axios from 'axios';
 
 export default {
   name: 'Container',
-  data() {
-    return {
-      projects: [],
-      baseUrl: 'http://127.0.0.1:8000',
-      apiUrl: {
-        projects: '/api/projects',
-      },
-    };
-  },
   components: {
     AppHeader,
-    AppMain,
     AppFooter,
-  },
-  methods: {
-    getProjects() {
-      axios
-        .get(this.baseUrl + this.apiUrl.projects)
-        .then((response) => {
-          console.log(response);
-          this.projects = response.data.data.data;
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    },
-  },
-  created() {
-    this.getProjects();
   },
 };
 </script>
--- src/pages/ProjectList.vue
+++ src/pages/ProjectList.vue
@@ -1,11 +1,60 @@
 <script>
+import ProjectCard from '../components/ProjectCard.vue';
+import axios from 'axios';
+
 export default {
   name: 'ProjectList',
+  data() {
+    return {
+      projects: [],
+      baseUrl: 'http://127.0.0.1:8000',
+      apiUrl: {
+        projects: '/api/projects',
+      },
+    };
+  },
+  components: {
+    ProjectCard,
+  },
+  methods: {
+    getProjects() {
+      axios
+        .get(this.baseUrl + this.apiUrl.projects)
+        .then((response) => {
+          console.log(response);
+          this.projects = response.data.data.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+  created() {
+    this.getProjects();
+  },
 };
 </script>
 
 <template>
-  <div>projects list</div>
+  <main class="bg-info">
+    <div class="container py-3">
+      <h1 class="text-center">Elenco dei Progetti</h1>
+      <div class="row mt-4">
+        <div class="col col-md-4 g-4" v-for="project in projects">
+          <ProjectCard :project="project" />
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+h1 {
+  color: green;
+  text-transform: uppercase;
+}
+.card {
+  border: 2px solid black;
+  background-color: lightgray;
+}
+</style>
