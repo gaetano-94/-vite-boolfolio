0e887cccb0df768fae72b30f239bfc2cec998954
gaetano-94 <gaetano.panico1994@gmail.com>
2024-02-18 13:14:10 +0100

    feat: import pages about

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 import AppHome from './pages/AppHome.vue';
 import ProjectList from './pages/ProjectList.vue';
+import AppAbout from './pages/AppAbout.vue';
 
 const router = createRouter({
   history: createWebHistory(),
@@ -15,6 +16,11 @@ const router = createRouter({
       path: '/blog',
       name: 'projects',
       component: ProjectList,
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: AppAbout,
     },
   ],
 });
