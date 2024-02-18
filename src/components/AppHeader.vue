e3d4f41c83facfa69d4565619b2da6adf0ae2158
gaetano-94 <gaetano.panico1994@gmail.com>
2024-02-18 13:14:37 +0100

    feat: refactor link

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -10,10 +10,27 @@ export default {
       <div>Logo</div>
       <nav>
         <ul class="list-unstyled m-0 p-0 gap-4 d-flex">
-          <li>Blog</li>
-          <li>Blog</li>
-          <li>Blog</li>
-          <li>Blog</li>
+          <li>
+            <router-link
+              :to="{ name: 'home' }"
+              class="text-white text-decoration-none"
+              >Home</router-link
+            >
+          </li>
+          <li>
+            <router-link
+              :to="{ name: 'projects' }"
+              class="text-white text-decoration-none"
+              >Projects</router-link
+            >
+          </li>
+          <li>
+            <router-link
+              :to="{ name: 'about' }"
+              class="text-white text-decoration-none"
+              >About</router-link
+            >
+          </li>
         </ul>
       </nav>
     </div>
