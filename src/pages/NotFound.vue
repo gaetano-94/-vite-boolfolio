7f62e5397e692a18cc622478b579842f1ece3f39
gaetano-94 <gaetano.panico1994@gmail.com>
2024-02-18 22:38:24 +0100

    feat: not-found

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  name: 'NotFound',
+};
+</script>
+
+<template>
+  <div class="text-center py-4 container">
+    <h2 class="text-uppercase">Spiacenti pagina non trovata</h2>
+    <h6>
+      <router-link :to="{ name: 'home' }" class="text-decoration-none"
+        >Torna all'home page</router-link
+      >
+    </h6>
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
