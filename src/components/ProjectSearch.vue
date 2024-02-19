31096c6e71a728287d65353cb19a58316bcb719b
gaetano-94 <gaetano.panico1994@gmail.com>
2024-02-19 16:47:33 +0100

    feat: project-search

---
+++ src/components/ProjectSearch.vue
@@ -0,0 +1,36 @@
+<script>
+import store from '../store';
+
+export default {
+  name: 'ProjectSearch',
+  data() {
+    return {
+      store,
+    };
+  },
+  emits: ['searchProject'],
+  methods: {
+    search() {
+      this.$emit('searchProject');
+    },
+  },
+};
+</script>
+
+<template>
+  <section id="project-search">
+    <form action="" @submit.prevent="search">
+      <div class="form-floating mb-2 mt-4">
+        <input
+          type="text"
+          class="form-control"
+          id="search-key"
+          placeholder="Cerca nei Progetti"
+          v-model="store.project.searchKey"
+        />
+        <label for="search-key">Cerca nei Progetti</label>
+      </div>
+      <button class="btn btn-primary">Cerca</button>
+    </form>
+  </section>
+</template>
